f4f70fb5fcb0d7ee9aa0ba276656a6dff4434ad6
Gabriel <g>
2019-10-02 10:42:42 -0400

    Object Avoidance Works

--- Practice/objectavoidance_v.2.py
+++ Practice/objectavoidance_v.2.py
@@ -3,7 +3,7 @@ import time
 
 speed = 20
 ultravalue = 255
-motorDif = [-340,340]
+motorDif = [-340,340,-140,140]
 
 BP = brickpi3.BrickPi3()
 
@@ -30,9 +30,31 @@ def turnLeft():
     BP.set_motor_power(BP.PORT_A, 0)
     BP.set_motor_power(BP.PORT_D, 0)
 
+def turn45right():
+    motorsPos = [BP.get_motor_encoder(BP.PORT_A),BP.get_motor_encoder(BP.PORT_D)]
+
+    while(BP.get_motor_encoder(BP.PORT_A) - motorsPos[0] > motorDif[2]):
+        BP.set_motor_power(BP.PORT_A, -speed)
+        BP.set_motor_power(BP.PORT_D, speed)
+    
+    BP.set_motor_power(BP.PORT_A, 0)
+    BP.set_motor_power(BP.PORT_D, 0)
+
+def turn45left():
+    motorsPos = [BP.get_motor_encoder(BP.PORT_A),BP.get_motor_encoder(BP.PORT_D)]
+
+    while(BP.get_motor_encoder(BP.PORT_A) - motorsPos[0] < motorDif[3]):
+        BP.set_motor_power(BP.PORT_A, speed)
+        BP.set_motor_power(BP.PORT_D, -speed)
+        # print(BP.get_motor_encoder(BP.PORT_A) - motorsPos[0])
+    
+    BP.set_motor_power(BP.PORT_A, 0)
+    BP.set_motor_power(BP.PORT_D, 0)
+
 def checkObject():
     loop = True
     while(loop):
+        print("repeating..")
         BP.set_motor_power(BP.PORT_A, -speed)
         BP.set_motor_power(BP.PORT_D, -speed)
         time.sleep(1.5)
@@ -48,7 +70,20 @@ def checkObject():
         if(uvalue < 30 or uvalue == 255.0):
              turnLeft()
         else:  
-            loop = False
+            turn45right()
+            try:
+                uvalue2 = BP.get_sensor(BP.PORT_4) # print the distance in CM
+            except brickpi3.SensorError as error:
+                print(error)
+            print(uvalue2)
+            if(uvalue2 < 75 or uvalue2 == 255.0):
+                print("You are in the if")
+                turnLeft()
+            else:
+                loop = False
+                print("You are in the else")
+
+            turn45left()
         
 try:
     while True:
@@ -61,6 +96,7 @@ try:
         if(ultravalue <= 20):
             turnLeft()
             checkObject()
+            print("you are now back here")
         else:
             BP.set_motor_power(BP.PORT_A, -speed)
             BP.set_motor_power(BP.PORT_D, -speed)
