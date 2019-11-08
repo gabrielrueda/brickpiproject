a4effa49853fca3875aa09c37851aa2711dac9bf
Gabriel <gabriel.i.rueda@gmail.com>
2019-11-08 09:31:21 -0800

    Progress

--- Projects/Testing/movinghead.py
+++ Projects/Testing/movinghead.py
@@ -5,22 +5,43 @@ speed = 20
 rightLimit = 0
 leftLimit = 0
 centreEncoder = 0
+currentEValue= 0
 
 BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be the BrickPi3 object.
 
 # pylint: disable=no-member
 BP.set_sensor_type(BP.PORT_4, BP.SENSOR_TYPE.EV3_ULTRASONIC_CM)
+def configUltra():    
+    try:
+        BP.get_sensor(BP.PORT_4)
+    except brickpi3.SensorError:
+        print("Configuring...")
+        error = True
+        while error:
+            time.sleep(0.1)
+            try:
+                BP.get_sensor(BP.PORT_4)
+                error = False
+            except brickpi3.SensorError:
+                error = True
+    print("Configured Utrasonic Sensor")
 
 try:
 
     centreEncoder = BP.get_motor_encoder(BP.PORT_B)
-    rightLimit = centreEncoder - 50
-    leftLimit = centreEncoder + 50
+    rightLimit = centreEncoder + 50
+    leftLimit = centreEncoder - 50
+    configUltra()
 
-    while(BP.get_motor_encoder(BP.PORT_B) < leftLimit):
-        BP.set_motor_power(BP.PORT_B, speed)
+    while(True):
+        if(currentEValue > leftLimit):
+            BP.set_motor_power(BP.PORT_B, speed)
+            currentEValue = BP.get_motor_encoder(BP.PORT_B)
+            print(currentEValue)
+        else:
+            break
         time.sleep(0.02)
-        print(BP.get_motor_encoder(BP.PORT_B))
+        BP.set_motor_power(BP.PORT_B,0)
 
 except KeyboardInterrupt: # the program gets interrupted by Ctrl+C on the keyboard.
     BP.reset_all() 
