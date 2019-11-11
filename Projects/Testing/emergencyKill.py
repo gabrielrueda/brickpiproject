7d1a34c8a654d9c449c4040272a4f10a84a8ba8b
Antonio <ant.iatonna@gmail.com>
2019-11-11 10:40:13 -0500

    Nothing

--- Projects/Testing/emergencyKill.py
+++
@@ -1,9 +0,0 @@
-import brickpi3
-
-BP = brickpi3.BrickPi3()
-
-try:
-    while (True):
-        print("PRESS CTRL + C TO KILL")
-except KeyboardInterrupt: # the program gets interrupted by Ctrl+C on the keyboard.
-    BP.reset_all() 
--- README.md
+++
@@ -1,2 +0,0 @@
-High School CO-OP students at the University of Windsor
-Includes all projects created
