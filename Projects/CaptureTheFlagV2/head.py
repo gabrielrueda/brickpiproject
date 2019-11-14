5174c4fb72be72a0936fe149590edda74b6b413b
Antonio <ant.iatonna@gmail.com>
2019-11-14 08:44:42 -0500

    Fixed Error

--- Projects/CaptureTheFlagV2/head.py
+++ Projects/CaptureTheFlagV2/head.py
@@ -2,26 +2,26 @@ import brickpi3
 import time
 
 BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be the BrickPi3 object.
-l_limit = 0
-r_Limit = 0
-c_Encoder = 0
+leftLimit = 0
+rightLimit = 0
+centreEncoder = 0
 speed = 60
 
 def limits(left, right):
-    c_Encoder = BP.get_motor_encoder(BP.PORT_B)
-    l_limit = BP.get_motor_encoder(BP.PORT_B) + left
-    r_limit = BP.get_motor_encoder(BP.PORT_B) - right
+    centreEncoder = BP.get_motor_encoder(BP.PORT_B)
+    leftLimit = BP.get_motor_encoder(BP.PORT_B) + left
+    rightLimit = BP.get_motor_encoder(BP.PORT_B) - right
 
 def returnCenter():
     print("Centre")
     currentEValue= BP.get_motor_encoder(BP.PORT_B)
-    if(currentEValue > c_Encoder):
-        while(currentEValue > c_Encoder):
+    if(currentEValue > centreEncoder):
+        while(currentEValue > centreEncoder):
             BP.set_motor_power(BP.PORT_B, -speed)
             currentEValue = BP.get_motor_encoder(BP.PORT_B)
             time.sleep(0.02)
     else:
-        while(currentEValue < c_Encoder):
+        while(currentEValue < centreEncoder):
             BP.set_motor_power(BP.PORT_B, speed)
             currentEValue = BP.get_motor_encoder(BP.PORT_B)
             time.sleep(0.02)
@@ -29,15 +29,15 @@ def returnCenter():
 def turnLeft():
     print("Left.")
     currentEValue= 0
-    while(currentEValue < l_limit):
+    while(currentEValue < leftLimit):
         BP.set_motor_power(BP.PORT_B, speed)
         currentEValue = BP.get_motor_encoder(BP.PORT_B)
         time.sleep(0.02)
 
 def turnRight():
-    print("Left Limit:" + str(l_limit))
+    print("Left Limit:" + str(leftLimit))
     currentEValue= 0
-    while(currentEValue > r_limit):
+    while(currentEValue > rightLimit):
         BP.set_motor_power(BP.PORT_B, -speed)
         currentEValue = BP.get_motor_encoder(BP.PORT_B)
         time.sleep(0.02)
