bd31b4eb23b0b6adf0b56c7190138b09330ef3c1
Antonio <ant.iatonna@gmail.com>
2019-11-29 10:53:26 -0500

    Bluetooth Now

--- Projects/Testing/ipConnection.py
+++ Projects/Testing/ipConnection.py
@@ -1,14 +1,23 @@
-# Run on brickpi
-import os
-hostname = "192.168.0.100"
-response = os.system("ping -c 1 " + hostname)
+# simple inquiry example
+from bluetooth import *
 
-#and then check the response...
-if response == 0:
-  print(hostname, 'is up!')
-else:
-  print(hostname, 'is down!')
+nearby_devices = bluetooth.discover_devices(lookup_names=True)
+print("Found {} devices.".format(len(nearby_devices)))
 
-#Green Bot - 192.168.0.104
-#Blue Bot - 192.168.0.100
-#To find ip address: write in terminal 'ifconfig' in the output look for 'inet' under 'wlan0' and the ip
+for addr, name in nearby_devices:
+    print("  {} - {}".format(addr, name))
+
+# # Run on brickpi
+# import os
+# hostname = "192.168.0.100"
+# response = os.system("ping -c 1 " + hostname)
+
+# #and then check the response...
+# if response == 0:
+#   print(hostname, 'is up!')
+# else:
+#   print(hostname, 'is down!')
+
+# #Green Bot - 192.168.0.104
+# #Blue Bot - 192.168.0.100
+# #To find ip address: write in terminal 'ifconfig' in the output look for 'inet' under 'wlan0' and the ip
