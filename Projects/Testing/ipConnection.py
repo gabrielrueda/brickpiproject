86c51f4cbb738359aa76c536a52b0b87174a231d
Antonio <ant.iatonna@gmail.com>
2019-12-02 08:30:14 -0500

    IP

--- Projects/Testing/ipConnection.py
+++ Projects/Testing/ipConnection.py
@@ -6,18 +6,3 @@ print("Found {} devices.".format(len(nearby_devices)))
 
 for addr, name in nearby_devices:
     print("  {} - {}".format(addr, name))
-
-# # Run on brickpi
-# import os
-# hostname = "192.168.0.100"
-# response = os.system("ping -c 1 " + hostname)
-
-# #and then check the response...
-# if response == 0:
-#   print(hostname, 'is up!')
-# else:
-#   print(hostname, 'is down!')
-
-# #Green Bot - 192.168.0.104
-# #Blue Bot - 192.168.0.100
-# #To find ip address: write in terminal 'ifconfig' in the output look for 'inet' under 'wlan0' and the ip
