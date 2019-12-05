f6191bc66a80265b8a4aa208031bd54295143db7
Gabriel <gabriel.i.rueda@gmail.com>
2019-12-05 09:34:06 -0500

    Robots can now connect

--- Projects/Testing/testSSH.py
+++ Projects/Testing/testSSH.py
@@ -1,21 +1,20 @@
 from pexpect import pxssh
 import getpass
-try:                                                            
-    s = pxssh.pxssh()
-    hostname = raw_input('hostname: ')
-    username = raw_input('username: ')
+try:
+    s = pxssh.pxssh()                                                   
+    hostname = "192.168.0.100"
+    username = "pi"
     password = getpass.getpass('password: ')
     s.login (hostname, username, password)
     s.sendline ('uptime')   # run a command
     s.prompt()             # match the prompt
     print(s.before)          # print everything before the prompt.
-    s.sendline ('ls -l')
+    s.sendline ('cd Python_Scripts/CTF\ -\ Testing/')
     s.prompt()
     print(s.before)
-    s.sendline('df')
+    s.sendline('sudo python main.py')
     s.prompt()
     print(s.before)
-    s.logout()
 except pxssh.ExceptionPxssh, e:
     print("pxssh failed on login.")
     print(str(e))
