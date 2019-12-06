3deb380fb189a374b9f3923510be93bff4ca9dcb
Antonio <ant.iatonna@gmail.com>
2019-12-06 10:00:46 -0500

    test

--- Projects/CTF - Testing/testSSH.py
+++ Projects/CTF - Testing/testSSH.py
@@ -1,19 +1,40 @@
+# from pexpect import pxssh
+# import getpass
+
+# def run():
+#     try:
+#         s = pxssh.pxssh()
+#         s.force_password = True                                                   
+#         hostname = "192.168.0.100"
+#         username = "pi"
+#         password = getpass.getpass('password: ')
+#         s.PROMPT= 'SSH> '
+#         s.login (hostname, username, password, original_prompt=r'@[^:]+:.*?/#?')
+#         s.sendline ('uptime')   # run a command
+#         s.prompt()             # match the prompt
+#         print(s.before)          # print everything before the prompt.
+#         s.sendline ('cd Python_Scripts/CTF\ -\ Testing/')
+#         s.prompt()
+#         print(s.before)
+#         s.sendline('sudo python main.py')
+#         s.prompt()
+#         print(s.before)
+#     except pxssh.ExceptionPxssh as error:
+#         print("pxssh failed on login.")
+#         print(error)
 from pexpect import pxssh
 import getpass
-
 def run():
     try:
         s = pxssh.pxssh()
-        s.force_password = True                                                   
         hostname = "192.168.0.100"
         username = "pi"
         password = getpass.getpass('password: ')
-        s.PROMPT= 'SSH> '
-        s.login (hostname, username, password, auto_prompt_reset=False)
-        s.sendline ('uptime')   # run a command
+        s.login(hostname, username, password)
+        s.sendline('uptime')   # run a command
         s.prompt()             # match the prompt
-        print(s.before)          # print everything before the prompt.
-        s.sendline ('cd Python_Scripts/CTF\ -\ Testing/')
+        print(s.before)        # print everything before the prompt.
+        s.sendline('cd Python_Scripts/CTF\ -\ Testing/')
         s.prompt()
         print(s.before)
         s.sendline('sudo python main.py')
