e38d55e78c7021bdc8a5e3097033b5ac39894ca8
Antonio <ant.iatonna@gmail.com>
2019-12-09 09:29:44 -0500

    Paramiko

--- Projects/CTF - Testing/testSSH.py
+++ Projects/CTF - Testing/testSSH.py
@@ -1,46 +1,34 @@
 # from pexpect import pxssh
 # import getpass
-
 # def run():
 #     try:
 #         s = pxssh.pxssh()
-#         s.force_password = True                                                   
-#         hostname = "192.168.0.100"
+#         hostname = "192.168.0.104"
 #         username = "pi"
 #         password = getpass.getpass('password: ')
 #         s.PROMPT= 'SSH> '
-#         s.login (hostname, username, password, original_prompt=r'@[^:]+:.*?/#?')
-#         s.sendline ('uptime')   # run a command
+#         s.login(hostname, username, password, port=22, auto_prompt_reset=False)
+#         s.sendline('uptime')   # run a command
 #         s.prompt()             # match the prompt
-#         print(s.before)          # print everything before the prompt.
-#         s.sendline ('cd Python_Scripts/CTF\ -\ Testing/')
+#         print(s.before)        # print everything before the prompt.
+#         s.sendline('cd Python_Scripts/CTF\ -\ Testing/')
 #         s.prompt()
 #         print(s.before)
 #         s.sendline('sudo python main.py')
 #         s.prompt()
 #         print(s.before)
+#         s.logout()
 #     except pxssh.ExceptionPxssh as error:
 #         print("pxssh failed on login.")
 #         print(error)
-from pexpect import pxssh
-import getpass
-def run():
-    try:
-        s = pxssh.pxssh()
-        hostname = "192.168.0.104"
-        username = "pi"
-        password = getpass.getpass('password: ')
-        s.login(hostname, username, password, port=22)
-        s.sendline('uptime')   # run a command
-        s.prompt()             # match the prompt
-        print(s.before)        # print everything before the prompt.
-        s.sendline('cd Python_Scripts/CTF\ -\ Testing/')
-        s.prompt()
-        print(s.before)
-        s.sendline('sudo python main.py')
-        s.prompt()
-        print(s.before)
-        s.logout()
-    except pxssh.ExceptionPxssh as error:
-        print("pxssh failed on login.")
-        print(error)
+
+import base64
+import paramiko
+key = paramiko.RSAKey(data=base64.b64decode(b'AAA...'))
+client = paramiko.SSHClient()
+client.get_host_keys().add('ssh.example.com', 'ssh-rsa', key)
+client.connect('ssh.example.com', username='strongbad', password='thecheat')
+stdin, stdout, stderr = client.exec_command('ls')
+for line in stdout:
+    print('... ' + line.strip('\n'))
+client.close()
