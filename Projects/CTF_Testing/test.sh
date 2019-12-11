948e520e89b45b505f16c851089aa578d165cc18
Antonio <ant.iatonna@gmail.com>
2019-12-11 09:15:15 -0500

    Bash Scripts

---
+++ Projects/CTF_Testing/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo Hello World!
+
+dos2unix testSSH.py
+
+dos2unix Robot.py
+
+sudo python main.py
+# python testSSH.py &
+# python Robot.py &
---
+++ Projects/Testing/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo Hello World!
+
+dos2unix testSSH.py
+
+dos2unix Robot.py
