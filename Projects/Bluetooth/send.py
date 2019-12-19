a79f271758592621aa7b30832b1455588a62a162
Gabriel <gabriel.i.rueda@gmail.com>
2019-12-19 09:34:07 -0500

    Added Bluetooth Test Files

---
+++ Projects/Bluetooth/send.py
@@ -0,0 +1,17 @@
+import bluetooth
+
+# nearby_devices = bluetooth.discover_devices(lookup_names=True)
+# print("Found {} devices.".format(len(nearby_devices)))
+# for addr, name in nearby_devices:
+#     print("  {} - {}".format(addr, name))
+
+serverMACAddress = 'B8:27:EB:55:C0:33'
+port = 3
+s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+s.connect((serverMACAddress, port))
+while 1:
+    text = raw_input() # Note change to the old (Python 2) raw_input
+    if text == "quit":
+        break
+    s.send(text)
+s.close()
