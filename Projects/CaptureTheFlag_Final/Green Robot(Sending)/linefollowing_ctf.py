bd0fa5add3ced4c7c32ac718b5f4d9436ad8a14b
Gabriel <gabriel.i.rueda@gmail.com>
2020-01-16 10:18:04 -0500

    Green Robot - Also not stuck

--- Projects/CaptureTheFlag_Final/Green Robot(Sending)/linefollowing_ctf.py
+++ Projects/CaptureTheFlag_Final/Green Robot(Sending)/linefollowing_ctf.py
@@ -1,5 +1,6 @@
 import time
 import brickpi3
+import drive
 
 BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be the BrickPi3 object.
 
@@ -11,14 +12,27 @@ speed = -30
 def linefollowing():
     BP.set_sensor_type(BP.PORT_3, BP.SENSOR_TYPE.EV3_COLOR_REFLECTED)
     sensor = 0
+    elapsedTime = 0
+    maxTime = 17
+    startTime = time.time()
     while(sensor < 90):
         try:
             sensor = BP.get_sensor(BP.PORT_3)
             # print(sensor)
         except brickpi3.SensorError as error:
             print(error)
-    
-        turning(sensor)
+        
+        elapsedTime = round(time.time() - startTime,1)
+        if(elapsedTime <= maxTime):
+            turning(sensor)
+        else:
+            drive.moveBackward()
+            time.sleep(2)
+            drive.moveFast()
+            time.sleep(0.33)
+            drive.stop()
+            break
+        time.sleep(0.02)
 
 def turning(sensor):
     p = 0.8
