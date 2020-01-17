1312aa37d7de2d5bebd57cff9933245398a05355
Gabriel <gabriel.i.rueda@gmail.com>
2020-01-17 10:46:05 -0500

    Changes Directory

--- Projects/CaptureTheFlag_Final/Green Robot(Sending)/connectSSH.py
+++ Projects/CaptureTheFlag_Final/Green Robot(Sending)/connectSSH.py
@@ -5,36 +5,21 @@ import brickpi3
 BP = brickpi3.BrickPi3()
 
 ssh_client =paramiko.SSHClient()
+
 def run():
-    # ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     r_ip = "192.168.0.100"
     r_username = "pi"
     r_password = "robots1234"
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     print("Trying to Connect...")
     ssh_client.connect(hostname=r_ip, username=r_username,password=r_password)
+    sftp = ssh_client.open_sftp()
+    sftp.chdir('Python_Scripts/CaptureTheFlag_Final')
     print("First Line")
-    # ssh_client.SFTPClient.chdir(path="cd Python_Scripts")
-    # stdin,stdout,stderr=ssh_client.exec_command("cd Python_Scripts/CTF_Testing")
-    # for line in stdout:
-    #     print('... ' + line.strip('\n'))
-    # print("Second Line")
     stdin,stdout,stderr=ssh_client.exec_command("python Robot.py") 
     for line in stderr:
         print('... ' + line.strip('\n'))
 
-def close():
-    print("reached")
-    print ("Terminating Process...")
-    stdin,stdout,stderr=ssh_client.exec_command("sudo python emergencyKill.py") 
-    print("Powering Down...")
-    stdin,stdout,stderr=ssh_client.exec_command("sudo poweroff")
-    ssh_client.close()
 
-try:
-    print("Starting Search....")
-    run()
-
-except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
-    pass
-    # close()
+print("Starting Search....")
+run()
